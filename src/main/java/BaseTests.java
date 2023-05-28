da032d8db75183e49f058610daef07f91537312a
Faisal Kabir <onlyfaisalkabir@gmail.com>
2023-05-29 00:21:50 +0600

    initial setup

---
+++ src/main/java/BaseTests.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class BaseTests {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com");
+        driver.manage().window().maximize();;
+    }
+    @Test
+    public static void main(String args[]) {
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}

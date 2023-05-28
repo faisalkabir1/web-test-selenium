6b9beafd13f407727d17418dc6fa4b71ae7d0c66
Faisal Kabir <onlyfaisalkabir@gmail.com>
2023-05-29 00:24:13 +0600

    Update BaseTests.java

--- src/main/java/BaseTests.java
+++ src/main/java/BaseTests.java
@@ -12,10 +12,10 @@ public class BaseTests {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com");
-        driver.manage().window().maximize();;
+        driver.manage().window().maximize();
     }
     @Test
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         BaseTests test = new BaseTests();
         test.setUp();
     }

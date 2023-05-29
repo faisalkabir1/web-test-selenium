8c90014ab3cbacdafa3c9e578043eea4bd12c12f
Faisal Kabir <onlyfaisalkabir@gmail.com>
2023-05-29 23:02:03 +0600

    added login page class

--- src/test/java/Login/LoginTests.java
+++ src/test/java/Login/LoginTests.java
@@ -1,4 +1,21 @@
 package Login;
 
-public class LoginTests {
+import Base.BaseTests;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTests extends BaseTests {
+    //private WebDriver driver;
+    @Test
+    public Void testlogin() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.ClickForm();
+        loginPage.setUsername("tomsmith");
+        loginPage.setPass("SuperSecretPassword");
+        loginPage.clickLogin();
+        Thread.sleep(5000);
+
+        return null;
+    }
+
 }
